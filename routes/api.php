3ae1a366aa7d25f3a5ca0cd13fa189f5e98da396
Mina <m.memar@tapsell.ir>
2024-06-16 20:51:17 +0330

    agent model

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\Agent\V1\AgentController;
 use App\Http\Controllers\Api\Vendor\V1\VendorController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/v1/')->group(function () {
     Route::apiResource('vendors', VendorController::class);
+    Route::apiResource('agents', AgentController::class);
 });
 
